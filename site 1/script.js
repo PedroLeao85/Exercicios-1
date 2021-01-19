fa87b0465c86cfb0bf68d79b6372b318f69d986b
Pedro Leão <phalves1@hotmail.com>
2021-01-18 21:16:26 -0300

    Adiciona objeto Date

--- site 1/script.js
+++ site 1/script.js
@@ -1,1 +1,25 @@
-alert('Olá')
+function carregar() {
+    var msg = window.document.getElementById('msg')
+    var img = window.document.getElementById('imagem')
+    var texto = window.document.getElementById('texto')
+    data = new Date()
+    var hora = data.getHours()
+    //var hora = 22
+    msg.innerHTML = `Agora são ${hora} horas.`
+    if (hora >= 0 && hora < 12) {
+        //bom dia
+        img.src = 'dia.png'
+        document.body.style.background = '#e2cd9f'
+        texto.innerHTML = 'Bom dia!!!!'
+    }else if (hora >= 12 && hora < 18){
+        //boa tarde
+        img.src = 'tarde.png'
+        document.body.style.background = '#b9846f'
+        texto.innerHTML = 'Boa tarde!!!!'
+    }else {
+        //boa noite
+        img.src = 'noite.png'
+        document.body.style.background = '#515154'
+        texto.innerHTML = 'Boa Noite!!!!'
+    }
+}
